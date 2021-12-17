8022749e2d2e3d778759ae0f00367e76833aa4d2
Logan Bonnesen <lbonnesen@gmail.com>
2021-12-16 23:12:21 -0700

    updated js comments, html, and screenshot

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ var today = moment().format("dddd, MMMM Do YYYY h:mm:ss a");
 $("#currentDay").text(today);
 console.log(today)
 
+
+// save input to local storage
 $(document).ready(function() {
     $(".saveBtn").on("click", function() {
         var textInput = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        var time = $(this).attr("id");
 
-        localStorage.setItem(time, textInput);
-        
+        localStorage.setItem(textInput, time);
+        console.log(textInput)
+    })
     
-});
+// function to compare current time with past, present, and future
 
 function getTime() {
     var currentTime = moment().hour();
@@ -35,18 +38,19 @@ function getTime() {
 
 }
 
+// pull input from local storage
 
-
-$("#text-9 .description").val(localStorage.getItem("09"));
-$("#text-10 .description").val(localStorage.getItem("10"));
-$("#text-11 .description").val(localStorage.getItem("11"));
-$("#text-12 .description").val(localStorage.getItem("12"));
-$("#text-13 .description").val(localStorage.getItem("13"));
-$("#text-14 .description").val(localStorage.getItem("14"));
-$("#text-15 .description").val(localStorage.getItem("15"));
-$("#text-16 .description").val(localStorage.getItem("16"));
-$("#text-17 .description").val(localStorage.getItem("17"));
+$("#text-9").val(localStorage.getItem("09"));
+$("#text-10").val(localStorage.getItem("10"));
+$("#text-11").val(localStorage.getItem("11"));
+$("#text-12").val(localStorage.getItem("12"));
+$("#text-13").val(localStorage.getItem("13"));
+$("#text-14").val(localStorage.getItem("14"));
+$("#text-15").val(localStorage.getItem("15"));
+$("#text-16").val(localStorage.getItem("16"));
+$("#text-17").val(localStorage.getItem("17"));
 
 
 getTime();
+
 })
